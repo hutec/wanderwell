5b39e3bb394302fe4d9feb168650206bcd91112a
Robin Hutmacher <robin.hutmacher@gmail.com>
2026-05-17 10:58:02 +0200

    backend: reuse schema.sql for startup schema validation

--- backend/main.go
+++ backend/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	_ "embed"
 	"fmt"
 	"io"
 	"log/slog"
@@ -31,69 +32,11 @@ func initDB(databasePath string) (*pgxpool.Pool, error) {
 	return pool, nil
 }
 
+//go:embed db/schema.sql
+var schemaSQL string
+
 func ensureSchema(pool *pgxpool.Pool) error {
-	schema := `
-		CREATE EXTENSION IF NOT EXISTS postgis;
-
-		CREATE TABLE IF NOT EXISTS athlete (
-			id BIGINT PRIMARY KEY,
-			firstname TEXT,
-			lastname TEXT,
-			expires_at INTEGER,
-			refresh_token TEXT,
-			access_token TEXT
-		);
-		CREATE TABLE IF NOT EXISTS route (
-    		id            BIGINT PRIMARY KEY,
-    		user_id       BIGINT NOT NULL,
-    		start_date    TIMESTAMPTZ NOT NULL,
-    		name          VARCHAR NOT NULL,
-    		elapsed_time  INTEGER NOT NULL,
-    		moving_time   INTEGER NOT NULL,
-    		distance      FLOAT NOT NULL,
-    		average_speed FLOAT NOT NULL,
-    		elevation     FLOAT NOT NULL,
-    		bounds        TEXT NOT NULL,
-    		sport_type    VARCHAR,
-    		geom          geometry(LineString, 4326),
-    		FOREIGN KEY (user_id) REFERENCES athlete(id)
-		);
-
-		-- Create spatial index
-        CREATE INDEX IF NOT EXISTS route_geom_idx ON route USING GIST (geom);
-
-        -- Create MVT function for user routes
-        CREATE OR REPLACE FUNCTION user_routes(z int, x int, y int, query_params json)
-		RETURNS bytea AS $$
-		DECLARE
-		  mvt bytea;
-		  uid bigint;
-		BEGIN
-		  uid := (query_params->>'user_id')::bigint;
-
-		  SELECT INTO mvt ST_AsMVT(tile, 'user_routes', 4096, 'geom')
-		  FROM (
-		    SELECT
-		      id,
-		      name,
-		      sport_type,
-		      distance,
-		      start_date,
-		      ST_AsMVTGeom(
-		        ST_Transform(geom, 3857),
-		        ST_TileEnvelope(z, x, y),
-		        4096, 64, true
-		      ) AS geom
-		    FROM route
-		    WHERE user_id = uid AND geom && ST_Transform(ST_TileEnvelope(z, x, y), 4326)
-		  ) tile;
-
-		  RETURN mvt;
-		END;
-		$$ LANGUAGE plpgsql STABLE PARALLEL SAFE;
-		`
-
-	_, err := pool.Exec(context.Background(), schema)
+	_, err := pool.Exec(context.Background(), schemaSQL)
 	if err != nil {
 		return fmt.Errorf("failed to create schema: %w", err)
 	}
